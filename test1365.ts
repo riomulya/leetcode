4e8f3368a242d849073bfda36237c22534aea1e0
RioMulya <riomulya75@gmail.com>
2024-01-15 19:14:01 +0700

    update

--- test1365.ts
+++ test1365.ts
@@ -1,16 +1,19 @@
 // 1365. How Many Numbers Are Smaller Than the Current Number
 
 function smallerNumbersThanCurrent(nums: number[]): number[] {
-    // Buat salinan array nums dan urutkan
     const sortedNums = [...nums].sort((a, b) => a - b);
-    console.log(sortedNums);
-    // Buat objek Map untuk memetakan nilai ke indeksnya
-    const numToIndex = new Map(sortedNums.map((num, index) => [num, index]));
-    console.log(numToIndex)
+    const numToCount = new Map<number, number>();
 
-    // Gunakan map untuk membuat array hasil
-    console.log(nums.map((num) => numToIndex.get(num) || 0))
-    return nums.map((num) => numToIndex.get(num) || 0);
+    for (let i = 0; i < sortedNums.length; i++) {
+        if (!numToCount.has(sortedNums[i])) {
+            numToCount.set(sortedNums[i], i);
+        }
+    }
+    console.log(numToCount);
+    console.log(sortedNums)
+    console.log(nums.map((num) => numToCount.get(num) || 0))
+    return nums.map((num) => numToCount.get(num) || 0);
 }
+
 
 console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]))
